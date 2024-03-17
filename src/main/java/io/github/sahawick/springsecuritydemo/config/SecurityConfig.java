1eda983fb07294d41869dafb3917cd6f653d07a9
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-03-17 13:46:34 +0530

    overrode user detail service

---
+++ src/main/java/io/github/sahawick/springsecuritydemo/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.github.sahawick.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
