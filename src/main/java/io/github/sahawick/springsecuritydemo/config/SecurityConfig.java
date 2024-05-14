47c5086d2acc35744f699b6910c1423e4ba1bdc8
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-05-14 09:37:48 +0530

    added password encoder and modified userDetailsService in securityConfig

--- src/main/java/io/github/sahawick/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/io/github/sahawick/springsecuritydemo/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package io.github.sahawick.springsecuritydemo.config;
 
+import io.github.sahawick.springsecuritydemo.mapper.MyUserMapper;
+import io.github.sahawick.springsecuritydemo.service.MyUserService;
+import io.github.sahawick.springsecuritydemo.service.impl.MyUserDetailService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,12 +11,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final MyUserService myUserService;
+
+    private final MyUserMapper myUserMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,13 +43,20 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+        return new MyUserDetailService(myUserService, myUserMapper);
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
